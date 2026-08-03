f911ae48a1e62fbd4135e53b0daa125d7c7d7b35
dixmains-svg <dixmains@gmail.com>
2026-08-03 15:39:15 +0100

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="TMF LOGISTICS",
-    page_icon="🚛",
+    page_icon="🚛🚛🚛🚛🚛",
     layout="wide",
     initial_sidebar_state="expanded"
 )
