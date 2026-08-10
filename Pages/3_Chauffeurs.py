378e05f8e419c0c7d23597018968cee0eeb93c7a
dixmains-svg <dixmains@gmail.com>
2026-08-10 15:49:41 +0100

    3_Chauffeurs.py

--- Pages/3_Chauffeurs.py
+++ Pages/3_Chauffeurs.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
-from database.database import (
+from database import (
     get_chauffeurs,
     ajouter_chauffeur,
     supprimer_chauffeur
