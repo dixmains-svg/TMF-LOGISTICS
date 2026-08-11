4a21b1d9e9e33c9de04e95b4f280ce0bafc52120
dixmains-svg <dixmains@gmail.com>
2026-08-11 10:15:46 +0100

    4_Clients.py

--- Pages/4_Clients.py
+++ Pages/4_Clients.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
-from database.database import (
+from database import (
     get_clients,
     ajouter_client,
     supprimer_client
