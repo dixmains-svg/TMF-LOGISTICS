8c57a3e83aa04c16f7bd2148c2007ff6adf46f17
dixmains-svg <dixmains@gmail.com>
2026-08-12 13:28:22 +0100

    4_Clients.py

--- Pages/4_Clients.py
+++ Pages/4_Clients.py
@@ -3,7 +3,11 @@ import pandas as pd
 from pathlib import Path
 from io import BytesIO
 
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+FICHIER_CLIENTS = BASE_DIR / "Data" / "Clients.xlsx"
 # ============================================================
 # CONFIGURATION
 # ============================================================
