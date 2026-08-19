aa6f0d0aa56799eea2647b1c9d0ca8609f978c1e
dixmains-svg <dixmains@gmail.com>
2026-08-19 11:57:42 +0100

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 
 import streamlit as st
-from pathlib import Path
+from login import afficher_login
+from app_home import afficher_home
 
-
-# ============================================================
-# CONFIGURATION
-# ============================================================
-
+# Configuration de la page
 st.set_page_config(
     page_title="TMF LOGISTICS",
     page_icon="🚛",
     layout="wide",
-    initial_sidebar_state="expanded"
 )
 
+# 1. Vérification : Si l'utilisateur n'est PAS connecté -> Afficher Login
+if not st.session_state.get("connecte", False):
+    afficher_login()
+    st.stop()  # Empêche l'affichage de la suite
+
+# 2. Si l'utilisateur EST connecté -> Afficher la page d'accueil
+afficher_home()
 
 # ============================================================
 # CHEMINS
